cec2a04a5acb75bec0170056b42cd6c21e14468c
Marcel Henggeler <marcel.henggeler@expersoft.com>
2022-08-04 14:15:36 +0200

    update hangman

--- src/minigames/Hangman.java
+++ src/minigames/Hangman.java
@@ -3,12 +3,13 @@ package minigames;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Hangman {
     public static void main(String[] args) {
+        // User selects a word
         System.out.println("Please select a word");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
         String word;
         try {
             word = reader.readLine();
@@ -16,6 +17,45 @@ public class Hangman {
             e.printStackTrace();
             return;
         }
-        String[] wordAsArray = {"word"};
+        // User guesses until no tries left or game won
+        String[] wordAsArray = word.split("");
+        int triesLeft = 5;
+        int correctTries = 0;
+        String[] foundCharacters = new String[wordAsArray.length];
+        while (triesLeft > 0) {
+            System.out.println("Guess:");
+            String character;
+            try {
+                character = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+            if (Arrays.asList(wordAsArray).contains(character)) {
+                System.out.println("correct");
+                if (Arrays.asList(foundCharacters).contains(character) == false) {
+
+                    foundCharacters[correctTries] = character;
+                    correctTries = correctTries + 1;
+                }
+            } else {
+                System.out.println("incorrect");
+                triesLeft = triesLeft - 1;
+                System.out.println("tries left:" + triesLeft);
+            }
+            boolean isSame = true;
+            for (int i = 0; i < wordAsArray.length; i++) {
+                if (Arrays.asList(foundCharacters).contains(wordAsArray[i]) == false) {
+                    isSame = false;
+                }
+            }
+            if (isSame) {
+                System.out.println("You won!");
+                triesLeft = 0;
+            }
+            if (triesLeft == 0){
+                System.out.println("no tries left!");
+            }
+        }
     }
 }
