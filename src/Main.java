3b10b70a8e47b6e6bb5608d402e278e8693400f6
Marcel Henggeler <marcel.henggeler@expersoft.com>
2022-08-10 12:58:14 +0200

    update Main

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,6 @@ public class Main {
     myMethod("Marcel");
     myMethod("John");
 
-    int[][] myImage = { { 1, 2, 3, 4 }, { 5, 6, 7, 2 } };
 
     int result = myMethod(3);
     System.out.println(result);
